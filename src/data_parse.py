bbc151059a5b38a62f3dc168686ecae80f3ffc77
gutsy47 <bestcom5055@gmail.com>
2024-06-08 05:05:19 +0300

    Improve: Cashflow parser optimization
    It turned out that iter_rows() returns data 1000 times faster than sheet[x][y]

--- src/data_parse.py
+++ src/data_parse.py
@@ -1,11 +1,8 @@
 from openpyxl import Workbook, load_workbook
 from openpyxl.worksheet.worksheet import Worksheet
-
 import pandas as pd
 
 from os import listdir
-
-from timeit import default_timer as timer
 
 
 def get_cashflow(path: str, is_header: bool = True) -> list:
@@ -13,7 +10,7 @@ def get_cashflow(path: str, is_header: bool = True) -> list:
 
     - Quarter: Число, получаемое как 10*Y + Q. Например, 20241 = 2024, 1 квартал
     - Category: Инвентарный/номенклатурный номер категории актива
-    - Code: TODO Код в каком-то справочнике. Каком?
+    - Code: Код в справочнике
     - Name: Наименование нефинансового актива
     - Measure: Единица измерения
     - Start Amount: Количество на первое число (01.01, 01.04, 01.07, 01.10)
@@ -31,73 +28,65 @@ def get_cashflow(path: str, is_header: bool = True) -> list:
     :return: list - Список вида [[header], [row1], [row2], ...]
     """
 
-    # Проверяем, что входящий путь является путём к книге
-    if not path.endswith(".xlsx") or path.split('/')[-1].startswith("~$"):
-        raise ValueError("Указан неверный путь: " + path)
+    # Сразу отсеиваем таблицы, которые не умеем читать, чтобы не тратить время на загрузку
+    if " 105 " not in path and " 21 " not in path and " 101 " not in path:
+        raise AttributeError("Таблица неизвестного счёта: " + path)
 
+    # Открываем единственный лист в книге
+    workbook: Workbook = load_workbook(filename=path, read_only=True)
+    sheet: Worksheet = workbook.active
+
+    # Заголовки и переменная будущей таблицы
     header = [
         "Quarter", "Category", "Code", "Name", "Measure", "Start Amount", "Start Cost", "Flow In Amount",
         "Flow In Cost", "Flow Out Amount", "Flow Out Cost", "End Amount", "End Cost"
     ]
     result = [header] if is_header else []
 
-    # Таблицы имеют три различные структуры в зависимости от номера категории (21, 101, 105)
+    # Таблицы имеют две различные структуры в зависимости от счета
     if " 105 " in path:
-        # Открываем единственный лист в книге
-        workbook: Workbook = load_workbook(filename=path)
-        sheet: Worksheet = workbook.active
-
-        # Получаем текущий квартал из 8 столбца вида 10*Y + Q
-        quarter_cell = sheet[1][7].value.split()
+        quarter_cell = sheet[1][7].value.split()  # Текущий квартал из ячейки (1,8) в виде 10*Y + Q
         quarter = int(quarter_cell[2]) + int(quarter_cell[-2]) * 10
 
-        current_category = None  # Отслеживает текущую категорию
-        for row in sheet.iter_rows(min_row=4, max_col=13):
-            if row[0].value:
+        current_category = None
+        for row in sheet.iter_rows(min_row=4, max_col=13, values_only=True):
+            if row[0]:
                 # Если есть ID, то это актив
-                result.append([quarter, current_category] + [cell.value for cell in row[2:]])
-            elif row[1].value:
+                result.append([quarter, current_category] + [cell for cell in row[2:]])
+            elif row[1]:
                 # Если есть категория, то это категория (elif из-за строки с итогами)
-                current_category = row[1].value.split('-')[0].strip()
-
-    elif " 21 " or " 101" in path:
-        # Открываем единственный лист в книге
-        workbook: Workbook = load_workbook(filename=path)
-        sheet: Worksheet = workbook.active
-
-        quarter_cell = sheet[2][0].value.split()
+                current_category = row[1].split('-')[0].strip()
+    else:
+        quarter_cell = sheet[2][0].value.split()  # Текущий квартал из ячейки (2,1) в виде 10*Y + Q
         quarter = int(quarter_cell[6]) + int(quarter_cell[-2]) * 10
 
-        current_category = None  # Отслеживает текущую категорию
-        i = 15  # Индекс изменяется нелинейно, так как данные об одном активе разбиты на четыре строки и есть категории
-        while i < sheet.max_row + 1:
-            if sheet[i][0].value and sheet[i][0].value[:3] in ["21.", "101"]:
-                # Если строка начинается с "21.", то это категория
-                current_category = sheet[i][0].value.strip()
-            elif sheet[i][0].value and sheet[i + 2][4].value:
+        current_category = None
+        i = 0  # Индекс изменяется нелинейно, так как данные об одном активе разбиты на четыре строки и есть категории
+        m = 2 if " 21 за 1" in path else 0  # Магическое число. Таблица за 1 квартал имеет два пустых столбца
+        rows = list(sheet.iter_rows(min_row=15, values_only=True))  # iter_rows намного быстрее обращения sheet[x][y]
+        while i < len(rows):
+            if rows[i][0] and (rows[i][0].startswith("21.") or rows[i][0].startswith("101.")):
+                # Если строка начинается с "21." | "101.", то это категория
+                current_category = rows[i][0].strip()
+            elif rows[i][0] and i < len(rows) - 2 and rows[i + 2][4]:
                 # Если есть имя, но нет номера "продукта для которого закупка", то это начало актива
-                m = 2 if " 21 за 1" in path else 0  # Магическое число. Таблица за 1 квартал имеет два пустых столбца
-                row = [
+                result.append([
                     quarter,  # Квартал
                     current_category,  # Категория
-                    sheet[i + 2][4].value,  # Код в справочнике
-                    sheet[i][0].value.strip(),  # Название
-                    "шт.",  # Единица измерения
-                    (sheet[i+1][10].value or 0) - (sheet[i+1][11].value or 0),  # Количество на начало периода
-                    (sheet[i][10].value or 0) - (sheet[i][11].value or 0),  # Стоимость на начало периода
-                    sheet[i+1][12 + m].value,  # Количество оборота дебет
-                    sheet[i][12 + m].value,  # Стоимость оборота дебет
-                    sheet[i+1][13 + m].value,  # Количество оборота кредит
-                    sheet[i][13 + m].value,  # Стоимость оборота кредит
-                    (sheet[i+1][14 + m].value or 0) - (sheet[i+1][15 + m].value or 0),  # Количество на конец периода
-                    (sheet[i][14 + m].value or 0) - (sheet[i][15 + m].value or 0)  # Стоимость на конец периода
-                ]
-                result.append(row)
+                    rows[i + 2][4],  # Код в справочнике
+                    rows[i][0].strip(),  # Название
+                    "ед",  # Единица измерения
+                    (rows[i + 1][10] or 0) - (rows[i + 1][11] or 0),  # Количество на начало
+                    (rows[i][10] or 0) - (rows[i][11] or 0),  # Стоимость на начало
+                    rows[i + 1][12 + m],  # Количество оборота дебет
+                    rows[i][12 + m],  # Стоимость оборота дебет
+                    rows[i + 1][13 + m],  # Количество оборота кредит
+                    rows[i][13 + m],  # Стоимость оборота кредит
+                    (rows[i + 1][14 + m] or 0) - (rows[i + 1][15 + m] or 0),  # Количество на конец
+                    (rows[i][14 + m] or 0) - (rows[i][15 + m] or 0)  # Стоимость на конец
+                ])
                 i += 3
             i += 1
-
-    else:
-        raise AttributeError("Неизвестная таблица: " + path)
 
     return result
 
@@ -105,23 +94,18 @@ def get_cashflow(path: str, is_header: bool = True) -> list:
 if __name__ == '__main__':
     # Пример получения данных
     data = []
+    is_first = True
     cashflow_paths = ["../data/raw/Обороты по счету/" + x for x in listdir("../data/raw/Обороты по счету")]
-    start = timer()
-    for i, x in enumerate(cashflow_paths):
+    for x in cashflow_paths:
         try:
-            start1 = timer()
-            data += get_cashflow(x, is_header=True if i == 0 else False)  # Берём заголовки только в первый раз
-            print(f"Прочитано {i + 1}/{len(cashflow_paths)} за {round(timer() - start1)} секунд: {x}")
-        except ValueError as error:
-            print(error)
-    print("Данные прочитаны. Время чтения: ", round(timer() - start), "секунд")
+            data += get_cashflow(x, is_header=is_first)
+            is_first = False
+        except PermissionError as err:
+            print(err)
 
     df = pd.DataFrame(data[1:], columns=data[0])
     pd.set_option("display.max_columns", None)
-    pd.set_option("display.max_rows", None)
     pd.set_option("display.width", None)
     print(df)
 
-    print("Идёт выгрузка в ../data/processed/cashflow.csv...")
     df.to_csv("../data/processed/cashflow.csv", index=False)
-    print("Выгрузка завершена.")
